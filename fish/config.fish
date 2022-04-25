9b7446f7e5bb0d793e57cc49e294dade47908f32
RayhaanT <tanweer.rayhaan@gmail.com>
2022-04-25 16:51:55 -0400

    tmux config

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 set -g theme_display_virtualenv yes
 set -g theme_newline_cursor yes
 set -g theme_newline_prompt '$ '
-set -gx PATH /home/rayhaan/bin /home/rayhaan/.nvm/versions/node/v17.0.1/bin /home/rayhaan/clang+llvm/bin /home/rayhaan/ccls/Release /home/rayhaan/.local/bin $PATH
+set -gx PATH /home/rayhaan/bin /home/rayhaan/.nvm/versions/node/v17.0.1/bin /home/rayhaan/clang+llvm/bin /home/rayhaan/ccls/Release /home/rayhaan/.local/bin /home/rayhaan/.cargo/bin $PATH
 
 set GPG_TTY (tty)
 export GPG_TTY
