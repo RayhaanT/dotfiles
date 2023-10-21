b705f334ebd152f3c520a1484fd67fc59c31516a
RayhaanT <tanweer.rayhaan@gmail.com>
2023-10-20 23:51:49 -0400

    Allow telescope.nvim version to update to fix bug

--- nvim/lua/packer-conf.lua
+++ nvim/lua/packer-conf.lua
@@ -7,7 +7,7 @@ return require('packer').startup(function(use)
 
     -- Telescope fuzzy finder
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
     -- Treesitter (Incremental AST)
