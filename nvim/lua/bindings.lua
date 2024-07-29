0f39bf843ef54fd93421041d7618470ba9a8e5a5
RayhaanT <tanweer.rayhaan@gmail.com>
2024-07-29 13:57:08 -0700

    Some new vim bindings

--- nvim/lua/bindings.lua
+++ nvim/lua/bindings.lua
@@ -7,14 +7,18 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Quickfix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-m>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
 
 -- Jump to buffer using bufferline tags
-vim.keymap.set("n", "gb", ":BufferLinePick<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "ft", ":BufferLinePick<CR>", { noremap = true, silent = true })
 
 -- Proper escaping from terminal buffers
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- New tab and close tab
+vim.keymap.set("n", "<leader>t", ":tab sp<CR>")
+vim.keymap.set("n", "<leader>k", ":tabc<CR>")
 
 vim.g.rustfmt_autosave = 1
 
@@ -27,3 +31,32 @@ vim.api.nvim_create_user_command(
     '!lualatex %',
     {bang = false}
 )
+
+-- disable lsp diagnostics
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
+
+-- swap header and source
+vim.keymap.set("n", "<C-b>", function()
+    if vim.bo.filetype == "cpp" then
+        local hname = vim.fn.expand('%')
+        print(name)
+        local fh = io.open(hname, "r")
+        if f ~= nil then
+            io.close(fh)
+            vim.cmd('edit ' .. hname)
+        else
+            hname = vim.fn.expand('%:r.hpp')
+            vim.cmd('edit ' .. hname)
+        end
+    else
+        local sname = vim.fn.expand('%:r.c')
+    end
+end)
