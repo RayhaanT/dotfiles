2e237c39d2c465112d13db8dfa5ed0047dadb4df
RayhaanT <tanweer.rayhaan@gmail.com>
2026-06-02 16:26:45 -0400

    Using nvim native treesitter. Plugin only for parser installation

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -36,10 +36,29 @@ vim.opt.updatetime = 300
 -- termguicolors
 vim.opt.termguicolors = true
 
--- syntax-sensitive folds
-vim.opt.foldmethod = "syntax"
+-- treesitter-based folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldenable = false
 vim.opt.foldlevel = 15
+
+-- Disable treesitter on opencl kernels
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+    pattern = "*.cl",
+    callback = function(ev)
+        vim.treesitter.stop(ev.buf)
+    end,
+})
+
+-- Disable treesitter on large files (>100KB)
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function(ev)
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(ev.buf))
+        if ok and stats and stats.size > 100 * 1024 then
+            vim.treesitter.stop(ev.buf)
+        end
+    end,
+})
 
 -- line at colum 80
 vim.opt.colorcolumn = "80"
